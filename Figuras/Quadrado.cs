5228cbff83f9648a9b2f1208884e69108e501cb2
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2022-04-12 16:30:36 -0300

    first commit

---
+++ Figuras/Quadrado.cs
@@ -0,0 +1,14 @@
+public class Quadrado
+{
+//Propriedades
+    public double Lado { get; set; }
+
+    //Atributo em maiusculo
+    public double Area {
+     get { return Lado * Lado; }
+    }
+    public double Perimetro{get => 4 * Lado;}
+    public Quadrado(double Lado) => Lado = Lado;
+
+}
+
